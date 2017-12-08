14214b7845faeb9056d9a03c8475d114a2cdd26b
Jay Allen <allen.jeremiah555@gmail.com>
2017-12-07 22:21:38 -0500

    completed modal style + functionality

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
-let mobileMenu = new MobileMenu();
+var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), "85%");
 new RevealOnScroll($('.testimonial'), "60%");
-let stickyHeader = new StickyHeader();
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
